62718b91a5e72e9cafc07837ee104cc7ba285493
Joe Ferguson <joe@infosiftr.com>
2015-02-02 10:07:13 -0800

    Stop & remove old ones, fix copy-pasta

--- start.sh
+++ start.sh
@@ -3,6 +3,11 @@ set -e
 
 dir="$(dirname "$(readlink -f $BASH_SOURCE)")"
 
+docker stop rawdns || true
+docker stop apt-cacher-ng || true
+docker rm rawdns || true
+docker rm apt-cacher-ng || true
+
 docker pull tianon/rawdns
 docker pull tianon/apt-cacher-ng
 
@@ -12,4 +17,6 @@ docker run -d --restart=always --name rawdns \
 	-v "$dir"/dns.json:/etc/rawdns.json:ro \
 	tianon/rawdns rawdns /etc/rawdns.json
 
-docker run -d --restart=always --name apt-cacher-ng tianon/rawdns
+docker run -d --restart=always --name apt-cacher-ng \
+	--dns 8.8.8.8 --dns 8.8.4.4 \
+	tianon/apt-cacher-ng
