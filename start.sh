cee69139799a7872d639c1d6857750bfadf5201a
Joe Ferguson <joe@infosiftr.com>
2017-02-10 16:00:59 -0800

    Fix rawdns in hocker

--- start.sh
+++ start.sh
@@ -15,7 +15,7 @@ else
 	docker run -d --restart=always --name rawdns \
 		-p 53:53/udp -p 53:53 \
 		-v /var/run/docker.sock:/var/run/docker.sock \
-		-v "$dir"/dns.json:/etc/rawdns.json:ro \
+		-v "$dir"/.rawdns.json:/etc/rawdns.json:ro \
 		tianon/rawdns rawdns /etc/rawdns.json
 
 	docker run -d --restart=always --name haproxy-sks \
